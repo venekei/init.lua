6af035810f8e7f4ae1948b6c5163dcf393e3c579
venekei <venekei@gmail.com>
2024-11-22 10:22:49 +0100

    Update mapKeys.lua

--- nvim/lua/vekni/mapKeys.lua
+++ nvim/lua/vekni/mapKeys.lua
@@ -1,13 +1,17 @@
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>=", "<cmd>normal gg=G<CR>")
-vim.keymap.set("n", "<leader>cb", "<cmd>bd!<CR>")
-vim.keymap.set("n", "<leader>sb", "<cmd>w<CR>bd<CR>")
-vim.keymap.set("n", "<leader>nb", "<cmd>bnext<CR>")
-vim.keymap.set("n", "<leader>pb", "<cmd>bprevious<CR>")
+vim.keymap.set("n", "<leader>bc", "<cmd>bd!<CR>")
+vim.keymap.set("n", "<leader>bs", "<cmd>w<CR>bd<CR>")
+vim.keymap.set("n", "<leader>bc", "<cmd>bnext<CR>")
+vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<CR>")
 
 -- diff tool keybindings --
-vim.api.nvim_set_keymap("n", "<leader>dn", "]c", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>dp", "[c", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>nc", "]c", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>pc", "[c", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>dg", "<cmd>diffget<cr>")
 vim.keymap.set("n", "<leader>dp", "<cmd>diffput<cr>")
+
+-- merge too keybindings --
+vim.keymap.set("n", "<leader>gl", "<cmd>diffget LOCAL<cr>")
+vim.keymap.set("n", "<leader>gr", "<cmd>diffget REMOTE<cr>")
